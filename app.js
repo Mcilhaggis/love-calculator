857ad17117144b4b66991f717ed4c19850b70f04
mcilhaggis <rachael.mcilhagga@live.co.uk>
2020-10-15 23:39:04 -0400

    adding random percentage generator

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-const game = ()=> { 
-    let percentage = 0%;
+
+var percentage = Math.floor(Math.random()*100);
+
+    //changes text on button click
+    function changeText() {
+    document.getElementById("compatability").innerHTML = percentage +"%";
+    }
+
+
+
+game();
+
+
+
+
+
+
+
