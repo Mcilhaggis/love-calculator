d7df83cca503e6fc7df076fe6c599087252c137d
mcilhaggis <rachael.mcilhagga@live.co.uk>
2020-10-16 09:16:19 -0400

    adding click event listerner to button to generate %

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 
-var percentage = Math.floor(Math.random()*100);
-
-    //changes text on button click
-    function changeText() {
-    document.getElementById("compatability").innerHTML = percentage +"%";
-    }
+    var percentage = Math.floor(Math.random()*100);
 
 
-
-game();
+    bton.addEventListener("click", function() {
+        document.getElementById("compatability").innerHTML = percentage +"%";
+    });
 
 
 
 
-
-
-
+    //outcomes - alerts or would prefer to have the text change of love at first sight...
+    /*function notice(){
+    if (percentage < 25){
+        alert("Big Yikes, move on!");
+    }else (percentage <50){
+        alert("Big Yikes, move on!");
+    }else (percentage > 50){
+        alert("Big Yikes, move on!");
+    }else (percentage > 85){
+        alert("Big Yikes, move on!"
+        )};8*/
